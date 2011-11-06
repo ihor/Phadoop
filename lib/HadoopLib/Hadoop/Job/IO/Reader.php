f1cd66758c85233da9ecd105fd92ff19a6cd0ef5
Ihor Burlachenko <ihor.burlachenko@gmail.com>
2011-11-06 15:36:28 +0200

    Small changes

--- lib/HadoopLib/Hadoop/Job/IO/Reader.php
+++ lib/HadoopLib/Hadoop/Job/IO/Reader.php
@@ -8,14 +8,14 @@ namespace HadoopLib\Hadoop\Job\IO;
 class Reader {
 
     /**
-	 * @return \HadoopLib\Hadoop\Job\IO\Data\Input|false
-	 */
-	public static function read() {
-		$line = fgets(STDIN);
-		if ($line !== false) {
-			return Data\Input::createFromString(trim($line));
-		}
+     * @return \HadoopLib\Hadoop\Job\IO\Data\Input|false
+     */
+    public static function read() {
+        $line = fgets(STDIN);
+        if ($line !== false) {
+            return Data\Input::createFromString(trim($line));
+        }
 
-		return false;
-	}
+        return false;
+    }
 }
