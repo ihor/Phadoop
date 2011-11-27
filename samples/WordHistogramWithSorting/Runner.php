b84b51b3c7b2e6c11dd17273b7574bc5de858c4e
Ihor Burlachenko <ihor.burlachenko@gmail.com>
2011-11-27 23:14:28 +0200

    Commented enabling debugging in the sorting example

--- samples/WordHistogramWithSorting/Runner.php
+++ samples/WordHistogramWithSorting/Runner.php
@@ -8,7 +8,7 @@ $classLoader->registerNamespace('HadoopLib', '../../lib');
 $classLoader->registerNamespace('WordHistogramWithSorting', '../');
 $classLoader->register();
 
-define('HADOOP_LIB_DEBUG', true);
+//define('HADOOP_LIB_DEBUG', true);
 
 $hadoop = new \HadoopLib\Hadoop('/usr/local/Cellar/hadoop');
 
